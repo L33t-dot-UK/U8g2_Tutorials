a6556888b921a25acac6acc57a969d54b9feee61
L33t.uk <56431771+L33t-dot-UK@users.noreply.github.com>
2025-11-14 08:37:45 +0000

    Update Blocking_Example.ino

--- Library/Examples/Blocking_Example/Blocking_Example.ino
+++ Library/Examples/Blocking_Example/Blocking_Example.ino
@@ -1,7 +1,7 @@
 /*
- * This example uses 3 sprites that take up 3952 bytes of dynamic memory
- * Make sure that your microcontroller has more than 10KB of RAM otherwise
- * this code will not work. In tutorial 8 we will store the spirtes in PROGMEM freeing up RAM.
+ * This example uses 3 sprites that take up 3952 bytes of memory.
+ * These are all stored in PROGMEM freeing up RAM.
+ * Once stored in PROGMEM the x.drawXBM() is replaced with x.drawXBMP()
  * 
  * The bird animation will play and the dragon animation will play both animations will block the micro controller and loop forever
  * 
@@ -440,4 +440,5 @@ void loop() {
    }
    DRAGON.resetAni(); //This will make the dragOn animation loop by setting the barEndX and barEndY values to true
 }
+
 
